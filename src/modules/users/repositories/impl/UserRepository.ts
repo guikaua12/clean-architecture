4f7bfdd690d6d876910253e78827a8870162791c
Approximations <datkidbosst@gmail.com>
2023-04-05 17:35:21 -0300

    MongoDB user model

--- src/modules/users/repositories/impl/UserRepository.ts
+++ src/modules/users/repositories/impl/UserRepository.ts
@@ -1,10 +1,18 @@
 import {IUserRepository} from '../IUserRepository';
 import {ICreateUserDTO} from '../../dtos/ICreateUserDTO';
 import {User} from '../../models/User';
+import {User as UserModel} from '../../database/models/User';
 
 export class UserRepository implements IUserRepository {
     async create({email, password}: ICreateUserDTO): Promise<User> {
-        return Promise.resolve(undefined);
+        const user = new UserModel({
+            email,
+            password
+        });
+
+        await user.save();
+
+        return {email, password} as User;
     }
 
 }
