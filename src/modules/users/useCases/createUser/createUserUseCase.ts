c5284e25b88fc03f4d8cfe05b160f86ccc8773aa
Approximations <datkidbosst@gmail.com>
2023-04-06 20:06:51 -0300

    classe de erro propria

--- src/modules/users/useCases/createUser/createUserUseCase.ts
+++ src/modules/users/useCases/createUser/createUserUseCase.ts
@@ -1,6 +1,7 @@
 import {ICreateUserDTO} from '../../dtos/ICreateUserDTO';
 import {IUserRepository} from '../../repositories/IUserRepository';
 import {User} from '../../models/User';
+import {AppError} from '../../../../shared/errors/AppError';
 
 export class CreateUserUseCase {
     constructor(private userRepository: IUserRepository) {
@@ -9,7 +10,7 @@ export class CreateUserUseCase {
     async execute({email, password}: ICreateUserDTO): Promise<User | null> {
         const userExists = await this.userRepository.findByEmail(email);
 
-        if(userExists) throw new Error('User already exists.');
+        if(userExists) throw new AppError(409, 'User already exists.');
 
         return await this.userRepository.create({email, password});
     }
