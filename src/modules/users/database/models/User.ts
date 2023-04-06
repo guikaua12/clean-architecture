9ab7dbc9e6c1fd27c22c336eca333a7ee6f268ea
Approximations <datkidbosst@gmail.com>
2023-04-05 23:22:29 -0300

    Adicionado um transform pra substituir o `_id` por `id` e remover o field `__v`

--- src/modules/users/database/models/User.ts
+++ src/modules/users/database/models/User.ts
@@ -4,6 +4,25 @@ import mongoose from 'mongoose';
 const schema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
+}, {
+    toJSON: {
+        virtuals: true,
+        transform: function (doc, ret) {
+            ret.id = ret._id;
+            delete ret.__v;
+            delete ret._id;
+            return ret;
+        }
+    },
+    toObject: {
+        virtuals: true,
+        transform: function (doc, ret) {
+            ret.id = ret._id;
+            delete ret.__v;
+            delete ret._id;
+            return ret;
+        }
+    }
 });
 
 export const UserModel = mongoose.model('user', schema);
