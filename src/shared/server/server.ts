e68079b9ca0df5d1de95a45195b62f18ff2c7be6
Approximations <datkidbosst@gmail.com>
2023-04-06 20:13:13 -0300

    adicionado tratamento de erro usando o `express-async-errors`

--- src/shared/server/server.ts
+++ src/shared/server/server.ts
@@ -1,13 +1,20 @@
 import express, {Request, Response, NextFunction} from 'express';
 import {router} from '../routes';
+import 'express-async-errors';
+import {AppError} from '../errors/AppError';
 
 
 const server = express();
 server.use(express.json());
 server.use(router);
 
+// error handling
+server.use((err: any , req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof AppError) {
+        return res.status(err.status).json({message: err.message});
+    }
 
-
-
+    res.status(500).json({message: 'Internal Server Error'});
+});
 
 export {server};
